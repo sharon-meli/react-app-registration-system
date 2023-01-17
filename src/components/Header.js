6f2ec147a940a5e603c8ed673f396f7f4e257ae8
sharon <sharonchepkirui665@gmail.com>
2023-01-18 01:09:36 +0300

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-// function Header(){
-//     return(
-//         <div>
-//  <h1>First Priority Medical Healthcare</h1>       
-//     <h2>Patients' registration system</h2>
+function Header(){
+    return(
+        <div>
+ <h1>First Priority Medical Healthcare</h1>       
+    <h2>Patients' registration system</h2>
 
-//         </div>
-//     )
-// }
-// export default Header;
+        </div>
+    )
+}
+export default Header;
